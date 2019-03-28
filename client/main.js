72700b92c8801edecd81fd9a9a59253c481102d7
Bridgitte <simone.p.bridgitte@hotmail.com>
2019-03-28 16:10:15 -0400

    edit, update, delete

--- client/main.js
+++ client/main.js
@@ -23,10 +23,52 @@ Template.addimgs.events({
 		var imgDesc = $("#imgDesc").val();
 			console.log("save", imgTitle, imgDesc, imgPath);
 				$("#addimgModal").modal("hide");
-		imagesDB.insert({"title":imgTitle, "path":imgPath, "description":imgDesc})
+		imagesDB.insert({"title":imgTitle, "path":imgPath, "description":imgDesc, "createdOn":Date()});
 	},
+	'click .js-cancelAdd'(){
+		$("#imgTitle").val('');
+		$("#imgPath").val('');
+		$("#imgDesc").val('');
+		$("#addImgPreview").attr('src');
+		$("#addImgModal").modal("hide");
+	},
+	'input #imgPath'(event){
+		var imgPath = $("#imgPath").val();
+		$("#addImgPreview").attr('src', imgPath);
+	},
+
 	'change #imgPath'(event){
 		var imgPath = $("#imgPath").val();
 		$("#addimgspreview").attr('src', imgPath);
+	}
+});
+
+Template.mainBody.events({
+	'click .js-delete'(){
+		var imgId = this._id;
+		$("#"+imgId).fadeOut('slow', function(){
+			imagesDB.remove({_id:imgId});
+
+		});
+
+	},
+	'click .js-edit'(event, instance){
+		var imgId = this._id;
+		$('#ImgPreview').attr('src',imagesDB.findOne({_id:imgId}).path);
+		$("#eimgTitle").val(imagesDB.findOne({_id:imgId}).title);
+		$("#eimgPath").val(imagesDB.findOne({_id:imgId}).path);
+		$("eimgDesc").val(imagesDB.findOne({_id:imgId}).description);
+		$('editModal').modal("show");
+	}
+});
+
+Template.editImg.events({
+	'click .js-updateImg'(){
+		var eId = $('#eId').val();
+		var imgTitle = $("#eimgTitle").val();
+		var imgPath = $("#eimgPath").val();
+		var imgDesc = $("#eimgDesc").val();
+		imagesDB.update({_id:eId}, {$set:{"title":imgTitle, "path":imgPath, "desc":imgDesc}});
+		$('#editImgModal').modal("hide");
 	}
 });
